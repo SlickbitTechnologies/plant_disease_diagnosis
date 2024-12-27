6f6379682aba0853f86325cc91abdcc76304dec4
krishnamrajuj87 <krishnamraju@banyanspace.com>
2024-12-27 12:38:08 +0530

    browser title changes

--- main.py
+++ main.py
@@ -5,10 +5,26 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+st.set_page_config(
+    page_title="Plant Disease Diagnosis",  # Browser tab title
+    page_icon='plant.png'  # Emoji or path to an image file
+)
+st.header(body="Plant Disease Diagnosis",
+          anchor=None,
+          help='AI-powered models and a user-friendly web interface to diagnose diseases in plants based on uploaded images. The application provides detailed analysis and suggestions for treatment, ensuring healthier plants and effective care.')
 
-st.header("Plant Disease Diagnosis")
+st.markdown(
+    """
+    <style>
+    .stFileUploader section  div  small { display: none; }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
-uploaded_file = st.file_uploader("Upload image to find disease of plant", type=["jpg", "jpeg", "png","webp"])
+uploaded_file = st.file_uploader("Upload image to find disease of plant (Max 250kb, files:jpg, jpeg, png, webp)",
+                                 type=["jpg", "jpeg", "png","webp"]
+                                 )
 
 genai.configure(api_key= os.getenv("GEMINI_API_KEY"))
 
@@ -27,17 +43,22 @@ model = genai.GenerativeModel(
 def upload_to_gemini(image_path, mime_type=None):
     file = genai.upload_file(image_path, mime_type=mime_type)
     return file
+def upload_image(image):
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
+
+    file_size_in_kb = uploaded_file.size / 1024
+
+    if file_size_in_kb > 250:
+        st.error("File size exceeds 250kb. Please upload a smaller file.")
+        return
+
     st.image(image, caption="Uploaded Image", use_container_width=True)
-
-    with open("temp_uploaded_image.jpeg", "wb") as f:
+    with open("temp.jpeg", "wb") as f:
         f.write(uploaded_file.getvalue())
 
     with st.spinner("Analyzing the image for plant diseases..."):
         try:
-            gemini_file = upload_to_gemini("temp_uploaded_image.jpeg", mime_type="image/jpeg")
+            gemini_file = upload_to_gemini("temp.jpeg", mime_type="image/jpeg")
 
             chat_session = model.start_chat(
                 history=[
@@ -49,7 +70,6 @@ if uploaded_file is not None:
                             "Highlights the disease name and important terms",
                             "Also mention at the end what is the plant and requirements to maintain it healthy."
                             "If no disease found return 'No disease found.'"
-
                         ],
                     }
                 ]
@@ -57,8 +77,12 @@ if uploaded_file is not None:
 
             response = chat_session.send_message("Please analyze the uploaded image.")
 
-            st.subheader("Diagnosis and Cure")
             st.write(response.text)
 
         except Exception as e:
             st.error(f"An error occurred: {e}")
+
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    upload_image(image)
